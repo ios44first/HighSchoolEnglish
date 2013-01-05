472835cbe545f5b96353ca4d571ce46c80675d58
Ibokan <ibokan@2-13.local>
2013-01-05 15:37:42 +0800

    Initial Commit

---
+++ HighSchoolEnglish/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  HighSchoolEnglish
+//
+//  Created by Ibokan on 13-1-5.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HighSchoolEnglish/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HighSchoolEnglish
+//
+//  Created by Ibokan on 13-1-5.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
